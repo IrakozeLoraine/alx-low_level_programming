8da6d7923d82551d57af7284f7299e58f1371f63
loraine <mukezwa@gmail.com>
2022-09-13 21:57:59 +0200

    renaming function name

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * is_alpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c - the character to check
  * Returns: 1 if c is a letter, 0 otherwise
  */
 
-int is_alpha(int c){
+int _isalpha(int c){
     return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
