0e9c51cb8dee3f32f5d4c29b779f1554b1f845c3
loraine <mukezwa@gmail.com>
2022-09-13 22:28:16 +0200

    refactor: reformulating print alphabet file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,16 @@
 void print_alphabet_x10(void)
 {
     int i;
+
     for (i = 0; i < 10; i++)
     {
         char alpha;
+        
         for (alpha = 'a'; alpha <= 'z'; alpha++)
         {
             _putchar(alpha);
         }
+        
     _putchar('\n');
     }
 }
