851166980070861985b606b75370abcd13e9583c
loraine <mukezwa@gmail.com>
2022-09-13 21:59:42 +0200

    reformat print alphabet code writing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-	for (i = 0; i < 10; i++)
+int i;
+for (i = 0; i < 10; i++)
+{
+	char alpha;
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
     {
-		char alpha;
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-            _putchar(alpha);
-        }
-        _putchar('\n');
-	}
+        _putchar(alpha);
+    }
+    _putchar('\n');
+}
 }
