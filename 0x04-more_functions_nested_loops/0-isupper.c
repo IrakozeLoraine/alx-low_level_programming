13f4494a9f75cc3d73a073417e7691dd00bffc7f
loraine <mukezwa@gmail.com>
2022-09-15 21:30:32 +0200

    fixing betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,9 @@
 /**
  * _isupper - checks for uppercase character
  * @c: print from this character 
- * @return 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    return (c>='A' && c<='Z');
+    return (c >= 'A' && c <= 'Z');
 }
